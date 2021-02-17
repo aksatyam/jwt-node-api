b99f3567022cb34d02109faa849be5e908400425
aksatyam <ashish.satyam@fancymonk.com>
2021-02-17 08:19:22 +0530

    private key integration

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-const privateKey = 'youraccesstokensecret';
+const { private_key } = require('./private_key')
 
 app.use(bodyParser.json());
 
 const authenticateJWT = async (req, res, next) => {
     const { authorization, public_key } = req.headers;
     if (authorization) {
-        await jwt.verify(authorization, `${privateKey}${public_key}`, (err, user) => {
+        await jwt.verify(authorization, `${private_key}${public_key}`, (err, user) => {
             if (err)
                 return res.status(403).send({ 'msg': 'UnAuthorized User' })
             else
@@ -25,10 +25,10 @@ app.post('/login', async (req, res) => {
     if (public_key) {
         // Read username and password from request body
         const { username, password } = req.body;
-        const accessToken = await jwt.sign(req.body, `${privateKey}${public_key}`);
+        const accessToken = await jwt.sign(req.body, `${private_key}${public_key}`);
         res.status(200).json({ username, accessToken });
     } else {
-        res.status(403).json({'msg': 'Inavlid Public Key'})
+        res.status(403).json({ 'msg': 'Inavlid Public Key' })
     }
 });
 
